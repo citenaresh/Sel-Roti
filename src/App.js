6f42f4937784ffac62be0b94b1f7d00b7c064652
citenaresh <73706869+citenaresh@users.noreply.github.com>
2021-04-29 22:19:49 +0800

    added checkout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 import AddProduct from './components/AddProduct';
 import Cart from './components/Cart';
@@ -7,8 +9,6 @@ import Login from './components/Login';
 import ProductList from './components/ProductList';
 
 import Context from "./Context";
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
 
 export default class App extends Component {
   constructor(props) {
@@ -21,77 +21,103 @@ export default class App extends Component {
     this.routerRef = React.createRef();
   }
 
-    //we need to make sure the user is loaded when the application starts up by setting the user on component mount;
-    async componentDidMount() {
-      let user = localStorage.getItem("user");
-      const products = await axios.get('http://localhost:3001/products');
-      user = user ? JSON.parse(user) : null;
-      this.setState({ user,products:products.data,cart });
+  async componentDidMount() {
+    let user = localStorage.getItem("user");
+    let cart = localStorage.getItem("cart");
+
+    const products = await axios.get('http://localhost:3001/products');
+    user = user ? JSON.parse(user) : null;
+    cart = cart? JSON.parse(cart) : {};
+
+    this.setState({ user,  products: products.data, cart });
+  }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
     }
-    addProduct = (product, callback) => {
-      let products = this.state.products.slice();
-      products.push(product);
-      this.setState({ products }, () => callback && callback());
-    };
+  }
 
-    addToCart = cartItem => {
-      let cart = this.state.cart;
-      if (cart[cartItem.id]) {
-        cart[cartItem.id].amount += cartItem.amount;
-      } else {
-        cart[cartItem.id] = cartItem;
-      }
-      if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
-        cart[cartItem.id].amount = cart[cartItem.id].product.stock;
-      }
-      localStorage.setItem("cart", JSON.stringify(cart));
-      this.setState({ cart });
-    };
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
 
-    removeFromCart = cartItemId => {
-      let cart = this.state.cart;
-      delete cart[cartItemId];
-      localStorage.setItem("cart", JSON.stringify(cart));
-      this.setState({ cart });
-    };
-    
-    clearCart = () => {
-      let cart = {};
-      localStorage.removeItem("cart");
-      this.setState({ cart });
-    };
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  };
 
-    login = async (email, password) => {
-      const res = await axios.post(
-        'http://localhost:3001/login',
-        { email, password },
-      ).catch((res) => {
-        return { status: 401, message: 'Unauthorized' }
-      })
-    
-      if(res.status === 200) {
-        const { email } = jwt_decode(res.data.accessToken)
-        const user = {
-          email,
-          token: res.data.accessToken,
-          accessLevel: email === 'admin@example.com' ? 0 : 1
-        }
-    
-        this.setState({ user });
-        localStorage.setItem("user", JSON.stringify(user));
-        return true;
-      } else {
-        return false;
-      }
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
     }
-    
-    logout = e => {
-      e.preventDefault();
-      this.setState({ user: null });
-      localStorage.removeItem("user");
-    };
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
 
-  
+  removeFromCart = cartItemId => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  clearCart = () => {
+    let cart = {};
+    localStorage.removeItem("cart");
+    this.setState({ cart });
+  };
+
+  checkout = () => {
+    if (!this.state.user) {
+      this.routerRef.current.history.push("/login");
+      return;
+    }
+
+    const cart = this.state.cart;
+
+    const products = this.state.products.map(p => {
+      if (cart[p.name]) {
+        p.stock = p.stock - cart[p.name].amount;
+
+        axios.put(
+          `http://localhost:3001/products/${p.id}`,
+          { ...p },
+        )
+      }
+      return p;
+    });
+
+    this.setState({ products });
+    this.clearCart();
+  };
 
   render() {
     return (
@@ -114,7 +140,7 @@ export default class App extends Component {
             aria-label="main navigation"
           >
             <div className="navbar-brand">
-              <b className="navbar-item is-size-4 ">SellRoti</b>
+              <b className="navbar-item is-size-4 ">Sell Roti</b>
               <label
                 role="button"
                 class="navbar-burger burger"
